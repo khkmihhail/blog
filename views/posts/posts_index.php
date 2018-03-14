251f1e01b614c598e40d60367ef3fad008f25fd2
khkmihhail <mihhail.matisinets@khk.ee>
2018-03-14 09:38:55 +0200

    lisasin linkid..

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,12 +1,14 @@
 <div class="container">
     <?foreach($posts as $post):?>
     <div class="col-md-12">
-        <h1><?=$post['post_subject']?></h1>
+        <a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>">
+            <h1><?=$post['post_subject']?></h1>
+        </a>
         <p><?=$post['post_text']?></p>
         <div>
+            <span class="label label-default"><?=$post['user_id']?></span>
             <span class="badge">Posted <?=$post['post_created']?></span>
             <div class="pull-right">
-                <span class="label label-default"><?=$post['user_id']?></span>
                 <span class="label label-primary">story</span>
                 <span class="label label-success">blog</span>
                 <span class="label label-info">personal</span>
---
+++ views/posts/posts_view.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <?foreach($posts as $post):?>
+        <div class="col-md-12">
+            <h1><?=$post['post_subject']?></h1>
+            <div>
+                <p><?=$post['post_text']?></p>
+            </div>
+            <div>
+                <span class="label label-default"><?=$post['user_id']?></span>
+                <span class="badge">Posted <?=$post['post_created']?></span>
+                <div class="pull-right">
+                    <span class="label label-primary">story</span>
+                    <span class="label label-success">blog</span>
+                    <span class="label label-info">personal</span>
+                    <span class="label label-warning">Warning</span>
+                    <span class="label label-danger">Danger</span>
+                </div>
+            </div>
+        </div>
+    <?endforeach?>
+</div>
